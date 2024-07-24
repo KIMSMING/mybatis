3a1dad51e18b4f14d8ea911e81e37f7cafd20836
KIMSMING <alstjdrla78@gmail.com>
2024-07-24 16:14:43 +0900

    수정 + 삭제
    수정 + 삭제

--- mybatisprojectapp/src/main/java/com/mybatis/mybatisprojectapp/Category/CategoryWebController.java
+++ mybatisprojectapp/src/main/java/com/mybatis/mybatisprojectapp/Category/CategoryWebController.java
@@ -4,10 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,7 +21,7 @@ public class CategoryWebController {
     }
 
     @GetMapping("/oldhtml/category_old")
-    public String category_Old(Model model){
+    public String categoryOld(Model model){
         try {
             List<ICategory> allList = this.categoryService.getAllList();
             model.addAttribute("itemList", allList);
@@ -49,4 +46,45 @@ public class CategoryWebController {
         return "redirect:category_old";
     }
 
+    @GetMapping("/oldhtml/category_old_view")
+    public String categoryOldView(@RequestParam Long id, Model model){
+        try {
+            ICategory byId = this.categoryService.findById(id);
+            if(byId == null){
+                return "redirect:category_old";
+            }
+            model.addAttribute("categoryDto", byId);
+        } catch (Exception ex){
+            log.error(ex.toString());
+        }
+        return "oldhtml/category_view";
+    }
+
+    @PostMapping("/oldhtml/category_old_update")
+    public String categoryOldUpdate(@ModelAttribute CategoryDto dto, Model model){
+        try{
+            if ( dto == null || dto.getName() == null || dto.getName().isEmpty()){
+                return "redirect:category_old";
+            }
+            this.categoryService.update(dto.getId(), dto);
+        } catch (Exception ex){
+            log.error(ex.toString());
+            return "oldhtml/category_old";
+        }
+        return "redirect:category_old";
+    }
+
+    @GetMapping("/oldhtml/category_old_del")
+    public String categoryOldDelete(@RequestParam Long id){
+        try {
+            ICategory byId = this.categoryService.findById(id);
+            if(byId == null){
+                return "redirect:category_old";
+            }
+            this.categoryService.remove(id);
+        } catch (Exception ex){
+            log.error(ex.toString());
+        }
+        return "redirect:category_old";
+    }
 }
